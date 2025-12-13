1bc0928e664b2a6d1ee6912004e5913ce0da98d8
jai shree <jaishree25eece@gmail.com>
2025-12-13 19:38:36 +0000

    address and mobile number also must be shown on the home page.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -59,7 +59,7 @@ export default function Header() {
               </a>
               <div className="flex items-center gap-2">
                 <MapPin className="w-4 h-4" />
-                <span>Bengaluru, Karnataka</span>
+                <span>Bommanahalli, Bengaluru</span>
               </div>
            </div>
           <Button variant="outline" asChild>
